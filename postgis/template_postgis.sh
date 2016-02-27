2bec8c0d7ff2cf40d22a45be5b625c3d0ae64da5
sabala12 <sabalah21@gmail.com>
2016-02-27 22:39:56 +0200

    stable

---
+++ postgis/template_postgis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Init script for template postgis
+#
+
+createdb -T template0 -O postgres -U postgres -E UTF8 template_postgis
+createlang plpgsql -U postgres -d template_postgis
+psql -U postgres template_postgis -c 'CREATE EXTENSION postgis;CREATE EXTENSION postgis_topology;CREATE EXTENSION schema_triggers;'
+ldconfig
