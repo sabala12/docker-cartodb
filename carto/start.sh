22ed63d7e9f3addd342ad9a159c46fc51de38917
Eran <sabalah21@gmail.com>
2016-10-02 23:16:34 +0300

    scripts update

--- carto/start.sh
+++ carto/start.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo -n -e "Enter 'y' to start setup.\n"; read SETUP
+echo -n -e "Enter 'y' to start cartodb setup.\n"; read SETUP
 if [[ "$SETUP" == "y" ]]; then
 
-        echo -n -e "Enter 'y' to run user initialization.\n"; read DB
+        #echo -n -e "Enter 'y' to run database initialization.\n"; read DB
+        read -t 3 -p "Enter 'y' to run database initialization.\n" DB;
         if [[ "$DB" == "y" ]]; then
+
+                cd /cartodb20
+
                 sh script/setup-db.sh
                 sh script/create_dev_user "${CARTO_USER}" "${CARTO_PASSWORD}" "${CARTO_EMAIL}"
                 sh script/modify-user-settings.sh
         fi
 
-        /cartodb20/script/run-services.sh
+        echo -n -e "Enter 'y' to run cartodb services.\n"; read SERVICES
+        if [[ "$SERVICES" == "y" ]]; then
+                /cartodb20/script/run-services.sh
+        fi
 fi
 
 /bin/bash
--- scripts/start/carto.sh
+++ scripts/start/carto.sh
@@ -5,16 +5,11 @@ persistent_storage="/data/cartodb"
 user_name="development"
 password="277336"
 domain="development"
-email="example@gmail.com"
-organization="development"
-#postgis_pass=475909
-#redis_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' redis)
+email="sabalah21@gmail.com"
+organization="org"
+#postgis_password=475909
 #postgis_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' postgis)
 
-#if [[ -z $redis_ip ]]; then
-#    echo "no redis ip!"; exit 0
-#fi
-#
 #if [[ -z $postgis_ip ]]; then
 #    echo "no postgis ip!"; exit 0
 #fi
@@ -26,6 +21,5 @@ sudo ~/Dev/docker-cartodb/carto/run-container.sh -n $container_name \
                                                  -d $domain \
                                                  -e $email \
                                                  -o $organization
-#                                              -a $redis_ip \
 #                                              -b $postgis_ip \
-#                                              -c $postgis_pass
+#                                              -c $postgis_password
